3d02e29c5b38c816fdcb2a11f30347e305bf8d3a
interestingLSY <interestingLSY@gmail.com>
2023-01-03 20:28:12 +0800

    Remove `log.h` in `naive.cpp`

--- answer/naive.cpp
+++ answer/naive.cpp
@@ -3,7 +3,6 @@
 	This solution just clicks all grids one by one
 */
 
-#include "log.h"
 #include "minesweeper_helpers.h"
 
 long N, K;
@@ -11,13 +10,10 @@ long N, K;
 int main() {
 	minesweeper_init(N, K);
 	Channel channel = create_channel();
-	long t = 0;
 	for (long r = 0; r < N; ++r) {
 		for (long c = 0; c < N; ++c) {
 			channel.click(r, c, true);
 		}
 	}
-	printf("%ld\n", t);
-	log("Done clicking\n");
 	return 0;
 }
